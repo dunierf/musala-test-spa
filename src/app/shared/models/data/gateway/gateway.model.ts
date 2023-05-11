11fe3c7802809fd2e26180ccdffc53319654c6d0
Duniesqui Rodriguez <dunierf@gmail.com>
2023-05-10 23:13:32 -0400

    Models

---
+++ src/app/shared/models/data/gateway/gateway.model.ts
@@ -0,0 +1,9 @@
+import { Device } from "../device/device.model";
+
+export interface Gateway {
+    id: number;
+    name: string;
+    serial: string;
+    ipv4: string;
+    devices?: Device[];
+}
