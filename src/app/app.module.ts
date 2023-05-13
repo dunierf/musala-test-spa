79a02e1809afaa600af11be33185c6c2b30db77f
Duniesqui Rodriguez <dunierf@gmail.com>
2023-05-13 15:41:12 -0400

    Ipv4 as simple input. Using regex pattern to validate it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,17 +14,24 @@ import { LayoutComponent } from './components/layout/layout/layout.component';
 import { ToolbarComponent } from './components/layout/toolbar/toolbar.component';
 import { ContentComponent } from './components/layout/content/content.component';
 import { SidebarComponent } from './components/layout/sidebar/sidebar/sidebar.component';
+import { MobileTabletSidebarComponent } from './components/layout/sidebar/mobile-tablet-sidebar/mobile-tablet-sidebar.component';
 import { MenuComponent } from './components/layout/menu/menu.component';
 
 // Pages
 import { NewGatewayPageComponent } from './components/pages/new-gateway-page/new-gateway-page.component';
 import { GatewaysCollectionPageComponent } from './components/pages/gateways-collection-page/gateways-collection-page.component';
 import { EditGatewayPageComponent } from './components/pages/edit-gateway-page/edit-gateway-page.component';
-import { MobileTabletSidebarComponent } from './components/layout/sidebar/mobile-tablet-sidebar/mobile-tablet-sidebar.component';
-import { CardComponent } from './components/ui/card/card.component';
-import { InputIpv4AddressComponent } from './components/ui/input-ipv4-address/input-ipv4-address.component';
+
+// Forms
 import { FormGatewayComponent } from './components/ui/forms/form-gateway/form-gateway.component';
 import { FormDeviceComponent } from './components/ui/forms/form-device/form-device.component';
+
+// Components
+import { CardComponent } from './components/ui/card/card.component';
+import { InputIpv4AddressComponent } from './components/ui/input-ipv4-address/input-ipv4-address.component';
+
+
+
 
 @NgModule({
   declarations: [
